89b33b40aff823e0ea2396e9e191d50b1882d832
Mustafa Zırhlı <mustafazirhli98@gmail.com>
2023-12-11 16:23:35 +0300

    component ve dosya yapıları oluşturuldu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-
+import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="container">
+
+      <Navbar />
+
 
     </div>
   );
